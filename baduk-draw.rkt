d323140f3a66575ad0527a5849108382f5cf954f
Maxwell Peterson <mson@localhost.localdomain>
2015-02-19 21:03:33 -0800

    wrote function to draw horiz lines, untested

--- baduk-draw.rkt
+++ baduk-draw.rkt
@@ -2,24 +2,32 @@
 (require racket/gui/base)
 
 (define board-size 19)
+(define frame-size 1000)
+(define margin-size 101)
+(define panel-size (- frame-size (* margin-size 2)))
+(define square-size (/ panel-size board-size))
+
 (define frame (new frame%
                    [label "board"]
-                   [width 1000]
-                   [height 1000]))
+                   [width frame-size]
+                   [height frame-size]))
 
 (define panel (new panel%
                    [parent frame]
                    [style (list 'border)]
-                   [horiz-margin 101]
-                   [vert-margin 101]))
+                   [horiz-margin-size 101]
+                   [vert-margin-size 101]))
 
 (new canvas%
      [parent panel]
      [paint-callback
-      (lambda (canvas dc)
-       ; draw 19 lines both ways. ( 
+      (lambda (canvas dc) ; draw 19 lines both ways.
+        (letrec ([draw-lines-horiz (lambda (y)
+                                     (cond
+                                       [(<= y 0) '()]
+                                       [else (send dc draw-line 0 y frame-size y)
+                                             (draw-lines-horiz (- square-size y))]))])
+          (draw-lines-horiz board-size)))])
 
-
-(send dc draw-line 0 0 1000 1000)
 
 (send frame show #t)
