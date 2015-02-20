02f8a9a31fb1b9f18115f423f286c0f0525223a6
Maxwell Peterson <mson@localhost.localdomain>
2015-02-20 00:59:55 -0800

    grid drawing works...! white stones can be placed..! and almost have functionality to snap stones in location to nearest gridpoint.

--- baduk-base.rkt
+++ baduk-base.rkt
@@ -1,22 +1,27 @@
 #lang racket
+(provide (except-out (all-defined-out)
+                     compass-dir-to-shifts
+                     liberty-directions))
+
 (define board-size 19)
 (define liberty-directions (list 'N 'S 'E 'W))
-(define all-directions (append liberty-directions (list 'NE 'NW 'SE 'SW)))
-(define compass-dir-to-shifts (hash 'N (lambda (row col) (list (+ row 1) col))
-                                    'S (lambda (row col) (list (- row 1) col))
-                                    'E (lambda (row col) (list row (- col 1)))
-                                    'W (lambda (row col) (list row (+ col 1)))
+(define compass-dir-to-shifts (hash 'N  (lambda (row col) (list (+ row 1) col))
+                                    'S  (lambda (row col) (list (- row 1) col))
+                                    'E  (lambda (row col) (list row       (- col 1)))
+                                    'W  (lambda (row col) (list row       (+ col 1)))
                                     'NE (lambda (row col) (list (+ row 1) (- col 1)))
                                     'NW (lambda (row col) (list (+ row 1) (+ col 1)))
                                     'SE (lambda (row col) (list (- row 1) (- col 1)))
                                     'SW (lambda (row col) (list (- row 1) (+ col 1)))))
 
+; returns the number of liberties gridpt has.
 (define (count-liberties gridpt)
   (length (filter (lambda (dir) (not (bordered-in-dir-by-color gridpt
                                                           dir
                                                           (opposite-color (gridpt-color gridpt)))))
                   liberty-directions)))
 
+; returns true if gridpt has zero liberties, false otherwise.
 (define (fully-surrounded gridpt)
   (= (count-liberties gridpt) 0))
 
@@ -33,6 +38,8 @@
                           (lambda (row) (build-list board-size
                                                (lambda (col) (gridpt row col #t null))))))
 
+; given white or black, returns the other color. Raises error if
+; color is not any of black or white or null.
 (define (opposite-color color)
   (cond
     [(equal? color 'white) 'black]
@@ -40,7 +47,8 @@
     [(null? color) null]
     [else (error "color is not black or white")]))
 
-(define (get-gridpt-by-coords row col)
+; returns the gridpt with coordinates row, col
+(define (get-gridpt-by-coords board row col)
   (list-ref (list-ref board row) col))
 
 ; returns true if the gridpt one space in direction dir from
@@ -53,14 +61,17 @@
          [gridpt-neighbor (get-gridpt-by-coords row col)])
     (and (not (gridpt-is-empty? gridpt)) (equal? color (gridpt-color gridpt-neighbor)))))
 
-(define (place-stone row col color)
+; place stone of color color at row, col. If a stone is already at location,
+; raises error.
+(define (place-stone board row col color)
   (let ([targetpt (get-gridpt-by-coords row col)])
     (cond
       [(not (gridpt-is-empty? targetpt)) (error (string-append "there is already a stone at " (~a targetpt)))]
       [else (set-gridpt-color! targetpt color)
             (set-gridpt-is-empty?! targetpt #f)])))
 
-(define (remove-stone row col)
+; take the stone at row, col off of board. If no stone is at location, does nothing.
+(define (remove-stone board row col)
   (let ([targetpt (get-gridpt-by-coords row col)])
     (set-gridpt-color! targetpt null)
     (set-gridpt-is-empty?! targetpt #t)))
--- baduk-draw.rkt
+++ baduk-draw.rkt
@@ -1,11 +1,12 @@
 #lang racket/gui
 (require racket/gui/base)
+(require "baduk-base.rkt")
 
-(define board-size 19)
 (define frame-size 1000)
 (define margin-size 101)
 (define panel-size (- frame-size (* margin-size 2)))
-(define square-size (/ panel-size board-size))
+(define padding board-size) ; margin around edges of go board
+(define square-size (/ (- panel-size padding) board-size))
 
 (define frame (new frame%
                    [label "board"]
@@ -15,19 +16,47 @@
 (define panel (new panel%
                    [parent frame]
                    [style (list 'border)]
-                   [horiz-margin-size 101]
-                   [vert-margin-size 101]))
+                   [horiz-margin (- margin-size padding)]
+                   [vert-margin (- margin-size padding)]))
 
-(new canvas%
-     [parent panel]
-     [paint-callback
-      (lambda (canvas dc) ; draw 19 lines both ways.
-        (letrec ([draw-lines-horiz (lambda (y)
-                                     (cond
-                                       [(<= y 0) '()]
-                                       [else (send dc draw-line 0 y frame-size y)
-                                             (draw-lines-horiz (- square-size y))]))])
-          (draw-lines-horiz board-size)))])
+(define go-canvas%
+  (class canvas%
+    (define/override (on-event ev)
+      (when (send ev button-down? 'left)
+        (let* ([x (send ev get-x)]
+               [y (send ev get-y)]
+               [center-x (- x (/ square-size 2))]
+               [center-y (- y (/ square-size 2))])
+          (send dc draw-ellipse center-x center-y square-size square-size))))
+    (super-new)))
 
+(define canvas (new go-canvas%
+                    [parent panel]
+                    [paint-callback ; draws board-size x board-size grid with margins around edges
+                     (lambda (canvas dc)
+                       (letrec ([draw-grid (lambda (x y)
+                                             (cond
+                                               [(< y 0) '()]
+                                               [else
+                                                (send dc draw-line padding y panel-size y)
+                                                (send dc draw-line x padding x panel-size)
+                                                (draw-grid (- x square-size) (- y square-size))]))])
+                         (draw-grid panel-size panel-size)))]))
+
+(define pixel-diagonal (sequence->list (in-range 0 panel-size square-size)))
+
+(define (coords--board->screen pixel-diagonal row col)
+  (cons (list-ref pixel-diagonal row) (list-ref pixel-diagonal col)))
+
+(define (coords--screen->board pixel-diagonal x y)
+  (letrec ([find-gridpt (lambda (pixel-diagonal px)
+                          (cond
+                            [(< px (car pixel-diagonal))
+                             (car pixel-diagonal)]
+                            [else
+                             (find-gridpt (cdr pixel-diagonal) px)]))])
+    (cons (find-gridpt x) (find-gridpt y))))
+
+(define dc (send canvas get-dc))
 
 (send frame show #t)
